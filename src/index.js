c13a569379b68306c2466b712445684268fce8cf
Days_Vinson <747142549@qq.com>
2021-01-29 18:14:49 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
 // 日历、日期语言--中文
 import zhCN from "antd/lib/locale/zh_CN";
 import { ConfigProvider, Layout } from "antd";
-import { BrowserRouter } from "react-router-dom";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 import "./index.css";
 
 // 导入动态渲染路由和菜单Link
@@ -12,87 +12,60 @@ import { CustomMenu, SwitchRoute } from "./router/router.js";
 // 导入Header部分组件
 import CustomHeader from "@view/pages/header/header.js";
 
-// 导入Login页面组件
+// 导入登录页面组件
 import Login from "@view/pages/login/login.js";
 
-// 导入REST API方法
-import { fetch, post, put, del } from "@/api/http.js";
+// 导入布局容器组件
+import { AdminSider, AdminHeader, AdminContent, AdminFooter } from "./layout.js";
+
+// 导入各种错误页面组件
+import { Error403, Error404, Error405 } from "@view/pages/error_pages.js";
+
+// 导入注册页面
+import { Register } from "@view/pages/register/register.js";
 
 
-class Container extends Component {
-    constructor(props) {
-        super(props);
-        this.changeLoginStatus = this.changeLoginStatus.bind(this);
-        this.state = {
-            isLoginStatus: true
-        };
-        this.$fetch = fetch.bind(this);  // get请求
-        this.$post = post.bind(this);  // post请求
-        this.$put = put;  // put请求
-        this.$del = del;  // delete请求
-        this.$path = "http://10.70.6.188:8001";  // 后台接口请求地址
-    }
+class Index extends React.Component {
 
-    async changeLoginStatus (loginStatus) {
-        await this.setState({
-            isLoginStatus: false
-        });
-        console.log(loginStatus, "这是登录状态");
-    }
-
-    render() {
+    render () {
         return (
             <ConfigProvider locale={zhCN}>
                 <BrowserRouter>
-                    {this.state.isLoginStatus ?
-                    <Layout style={{ height: "100%" }}>
-                    <Layout.Sider
-                        style={{
-                            overflow: "auto",
-                            height: "100vh",
-                            position: "fixed",
-                            left: 0
-                        }}>
-                        <div className="logo">
-                            <img
-                                src={ require("./static/images/login_tray.png").default }
-                                alt="图标"/>
-                        </div>
-                        {/* 动态渲染菜单 */}
-                        <CustomMenu />
-                    </Layout.Sider>
+                    <Switch>
+                        <Route path="/login" exact component={Login} />
+                        <Route path="/403" exact component={Error403} />
+                        <Route path="/404" exact component={Error404} />
+                        <Route path="/405" exact component={Error405} />
+                        <Route path="/register" exact component={Register} />
+                        <Route path="/" render={() =>
+                            <Layout style={{ height: "100%" }}>
+                                <AdminSider>
+                                    {/* 动态渲染菜单 */}
+                                    <CustomMenu />
+                                </AdminSider>
 
-                    <Layout
-                        className="site-layout"
-                        style={{ marginLeft: 200 }}>
-                        <Layout.Header
-                            className="site-layout-background"
-                            style={{ paddingLeft: "16px", height: "110px" }}>
-                            {/* 渲染Header部分的自定义头部,传入判断用户是否登录状态标记 */}
-                            <CustomHeader isLoginStatus={this.state.isLoginStatus} changeLoginStatus={this.changeLoginStatus} />
-                        </Layout.Header>
-                        <Layout.Content
-                            className="site-layout-background"
-                            style={{
-                                margin: "24px 16px 0",
-                                overflow: "auto"
-                            }}>
-                            {/* 动态渲染路由 */}
-                            <SwitchRoute />
-                        </Layout.Content>
-                        <Layout.Footer
-                            className="footer"
-                            style={{ textAlign: "center" }}>
-                            @copyright Wilson 2021
-                        </Layout.Footer>
-                    </Layout>
-                </Layout>
-                    : <Login />   // 条件判断渲染Login页面组件
-                    }
+                                <Layout className="custom-layout">
+                                    <AdminHeader>
+                                        {/* 渲染Header部分的自定义头部,传入判断用户是否登录状态标记 */}
+                                        <CustomHeader />
+                                    </AdminHeader>
+
+                                    <AdminContent>
+                                        {/* 动态渲染路由 */}
+                                        <SwitchRoute />
+                                    </AdminContent>
+
+                                    <AdminFooter>
+                                        <p>@copyright Wilson 2021</p>
+                                    </AdminFooter>
+                                </Layout>
+                            </Layout>
+                        } />
+                    </Switch>
                 </BrowserRouter>
             </ConfigProvider>
         );
     }
 }
 
-ReactDOM.render(<Container />, document.getElementById("root"));
+ReactDOM.render(<Index />, document.getElementById("root"));
