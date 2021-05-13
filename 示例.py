3f363b823174736fc1818f8a8204ff88209fce20
Days_Vinson <747142549@qq.com>
2021-05-13 17:40:43 +0800

    Update 示例.py

--- 示例.py
+++ 示例.py
@@ -35,3 +35,64 @@ filenames = os.listdir(r"D:\DownloadPackage\test")  # 获取该目录
 # [1:-1]截取掉圆括号,转换数据类型
 filenames.sort(key=lambda x: int(regex.search(x).group(0)[1:-1]), reverse=True)  # True为倒序，False为倒序
 print(filenames)
+
+
+"""" 分行,优化多进程 """"
+# -*- coding:utf-8 -*-
+#!/usr/bin/env python
+# Author: JISO
+# Email: 747142549@qq.com
+# File: pic_optimize.py
+import multiprocessing
+import os
+import pandas as pd
+
+
+def file_processing(folder_path):
+    """
+    文件处理函数,传入要处理目录的目标路径
+    D:\\DownloadPackage
+    :param folder_path: 文件夹路径
+    :return: DataFrame对象
+    """
+
+    file_name_list = os.listdir(folder_path)  # 获取该目录
+    f_df = pd.DataFrame({"file_name": file_name_list})
+    f_df["goods_name"], f_df["file_index"], f_df["file_extension"] = f_df["file_name"].str\
+        .split("[(|（]([0-9]*?)[)|）]").str
+    f_df["file_index"] = f_df["file_index"].astype("int")
+    
+    # # 进行分组排序,按照商品名称进行分类,
+    # df = df.groupby("goods_name", sort=False) \
+    #     .apply(lambda x: x.sort_values("file_index", ascending=True)) \
+    #     .reset_index(drop=True)
+    return df
+    
+    
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    # 创建5个进程,如果处理任务大于进程数量,那么处理任务会等待子进程释放
+    pool = multiprocessing.Pool(processes=5)
+    
+    # 获取要处理的DataFrame对象
+    df = file_processing(r"D:\DownloadPackage\test")
+    
+    # 遍历分组
+    for goods_name, group in df.groupby("goods_name", sort=False):
+        # 提取所有包含该商品名称的数据,返回DataFrame对象
+        goods_df = df[df["goods_name"] == goods_name]
+
+        # 子进程开启异步非阻塞模式
+        pool.apply_async(main, (goods_df, ))
+
+    # 关闭进程池,不在接受新任务
+    pool.close()
+    # 阻塞主进程
+    pool.join()
+
+    print("处理完成")
+
+
